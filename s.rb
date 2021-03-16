bc2bfd4f40785cd220091e89ec9833c2778c9377
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-03-16 13:08:10 +0600

    Total products automation successfully compiled and working with wait until feature

--- s.rb
+++ s.rb
@@ -6,15 +6,20 @@ require 'csv'
 require 'selenium-webdriver'
 require 'language_converter'
 def scraper
-
-# driver = Selenium::WebDriver.for :chrome
-# driver.navigate.to "https://www.prothoma.com/publisher/%E0%A6%85%E0%A6%A8%E0%A6%A8%E0%A7%8D%E0%A6%AF%E0%A6%BE-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%95%E0%A6%BE%E0%A6%B6%E0%A6%A8%E0%A7%80?page=1"
-# a = driver.find_elements(:class, "product-amount")[0].text.split(" ")[0]
-# total_product = lc(a,'en','bn').to_i
-# byebug
-
 puts "Give the URL"
 url = gets.chomp
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "#{url}"
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until {  driver.find_elements(:class, "product-amount")[0].text.split(" ")[0] !=nil }
+a = driver.find_elements(:class, "product-amount")[0].text.split(" ")[0]
+
+total_products = lc(a,'en','bn').to_i
+
+
+
+
+
 
 unparsed_page = HTTParty.get(url)
 parsed_page = Nokogiri::HTML(unparsed_page.body)
@@ -25,7 +30,7 @@ products = parsed_page.css('div.product_single')
 
 per_page = products.count
 page = 1
-total_products = 214
+
 
 last_page = (total_products.to_f / per_page.to_f).ceil
 
@@ -34,7 +39,9 @@ publisher =parsed_page.css('div.media-body').text.gsub(/\n/," ").strip
 
 while page <= last_page
   pagination_url =
-  "https://www.prothoma.com/publisher/%E0%A6%85%E0%A6%A8%E0%A6%A8%E0%A7%8D%E0%A6%AF%E0%A6%BE-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%95%E0%A6%BE%E0%A6%B6%E0%A6%A8%E0%A7%80?page=#{page}"
+  "https://www.prothoma.com/publisher/%E0%A6%85%E0%A6%A8%E0%A7%81%E0%A6%AA%E0%A6%AE-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%95%E0%A6%BE%E0%A6%B6%E0%A6%A8%E0%A7%80?page=#{page}"
+  a= "https://www.prothoma.com/publisher/%E0%A6%85%E0%A6%A8%E0%A6%A8%E0%A7%8D%E0%A6%AF%E0%A6%BE-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%95%E0%A6%BE%E0%A6%B6%E0%A6%A8%E0%A7%80?page=#{page}"
+  byebug
   pagination_unparsed_page = HTTParty.get(pagination_url)
   pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page.body)
   pagination_products = pagination_parsed_page.css('div.product_single')
