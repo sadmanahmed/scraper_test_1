966359600856008dd0f802b83d88b1c2a7e4a9f7
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-03-17 13:34:51 +0600

    Publisher list added in the CRV format with url and title

--- s.rb
+++ s.rb
@@ -6,80 +6,123 @@ require 'csv'
 require 'selenium-webdriver'
 require 'language_converter'
 def scraper
-puts "Give the URL"
-url = gets.chomp
+
+#puts "Give the URL"
+#url = gets.chomp
+url ="https://www.prothoma.com/publisher"
 url= url+"?page=1"
-#byebug
-driver = Selenium::WebDriver.for :chrome
-driver.navigate.to "#{url}"
-wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-wait.until {  driver.find_elements(:class, "product-amount")[0].text.split(" ")[0] !=nil }
-a = driver.find_elements(:class, "product-amount")[0].text.split(" ")[0]
-
-total_products = lc(a,'en','bn').to_i
-
-
-
-
-
-
 unparsed_page = HTTParty.get(url)
 parsed_page = Nokogiri::HTML(unparsed_page.body)
+pub_list = parsed_page.css('div.category_list_single_block')
 
-product_array = Array.new
+publisher_array = Array.new
 
-products = parsed_page.css('div.product_single')
+pub_first_page = 1
+pub_last_page = 22
+a =url
+while pub_first_page <= pub_last_page
 
-per_page = products.count
-page = 1
-
-
-last_page = (total_products.to_f / per_page.to_f).ceil
-
-n=0
-publisher =parsed_page.css('div.media-body').text.gsub(/\n/," ").strip
-a = url
-while page <= last_page
-  # pagination_url =
-  # "https://www.prothoma.com/publisher/%E0%A6%85%E0%A6%A8%E0%A7%81%E0%A6%AA%E0%A6%AE-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%95%E0%A6%BE%E0%A6%B6%E0%A6%A8%E0%A7%80?page=#{page}"
-
-  x= a.delete_suffix(a[-1]) + "#{page}"
+  x= a.delete_suffix(a[-1]) + "#{pub_first_page}"
 
   pagination_url = x
 
-  pagination_unparsed_page = HTTParty.get(pagination_url)
-  pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page.body)
-  pagination_products = pagination_parsed_page.css('div.product_single')
+  unparsed_page = HTTParty.get(pagination_url)
+  parsed_page = Nokogiri::HTML(unparsed_page.body)
 
-  pagination_products.each  do|product|
-    product ={
-      title: product.css('h2').text,
-      author: product.css('h3').text,
-      original_price: product.css('span.price_2').text,
-      discounted_price: product.css('span.price_1').text,
-      url: product.css('a')[0].attributes["href"].text
-    }
-      product_array << product
-  end
-  page += 1
-end
-temp = {
-      title: " ",
-      author: "",
-      original_price: " ",
-      discounted_price: " ",
-      url: " "
-    }
+  pagination_pub_list = parsed_page.css('div.category_list_single_block')
 #byebug
+  pagination_pub_list.each do |publisher|
+    publisher = {
+    name: publisher.css('div.category_info_ct_block').text,
+    url: publisher.css('a')[0].attributes["href"].value
+
+  }
+  publisher_array << publisher
+
+end
+pub_first_page += 1
+
+end
+      CSV.open("csv/publisher.csv",'w') do |csv|
+        csv << ['Title','URL']
+        publisher_array.each do |product|
+          #product = temp.merge(product)
+          csv << CSV::Row.new(product.keys,product.values)
+        end
+      end
 
 
-  CSV.open("#{publisher}.csv",'w') do |csv|
-    csv << ['Title','Author','Original Price','Discounted Price','URL']
-    product_array.each do |product|
-      product = temp.merge(product)
-      csv << CSV::Row.new(product.keys,product.values)
-    end
-  end
-  driver.quit
+
+  # publisher_array.each do |pub_url|
+  #   # puts "Give the URL"
+  #   # url = gets.chomp
+
+  #   url= pub_url.values[0] +"?page=1"
+  #   #byebug
+  #   driver = Selenium::WebDriver.for :chrome
+  #   driver.navigate.to "#{url}"
+  #   wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  #   wait.until {  driver.find_elements(:class, "product-amount")[0].text.split(" ")[0] !=nil }
+  #   a = driver.find_elements(:class, "product-amount")[0].text.split(" ")[0]
+
+  #   total_products = lc(a,'en','bn').to_i
+
+
+  #   unparsed_page = HTTParty.get(url)
+  #   parsed_page = Nokogiri::HTML(unparsed_page.body)
+  #   product_array = Array.new
+  #   products = parsed_page.css('div.product_single')
+  #   per_page = products.count
+  #   page = 1
+  #   if total_products > 0
+  #     last_page = (total_products.to_f / per_page.to_f).ceil
+  #   else
+  #     last_page= page
+  #   end
+  #   #last_page = (total_products.to_f / per_page.to_f).ceil
+
+  #   n=0
+  #   publisher =parsed_page.css('div.media-body').text.gsub(/\n/," ").strip
+  #   a = url
+  #   while page <= last_page
+
+  #     x= a.delete_suffix(a[-1]) + "#{page}"
+
+  #     pagination_url = x
+
+  #     pagination_unparsed_page = HTTParty.get(pagination_url)
+  #     pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page.body)
+  #     pagination_products = pagination_parsed_page.css('div.product_single')
+
+  #     pagination_products.each  do|product|
+  #       product ={
+  #         title: product.css('h2').text,
+  #         author: product.css('h3').text,
+  #         original_price: product.css('span.price_2').text,
+  #         discounted_price: product.css('span.price_1').text,
+  #         url: product.css('a')[0].attributes["href"].text
+  #       }
+  #         product_array << product
+  #     end
+  #     page += 1
+  #   end
+  #   temp = {
+  #         title: " ",
+  #         author: " ",
+  #         original_price: " ",
+  #         discounted_price: " ",
+  #         url: " "
+  #       }
+  #   #byebug
+  #     CSV.open("csv1/#{publisher}.csv",'w') do |csv|
+  #       csv << ['Title','Author','Original Price','Discounted Price','URL']
+  #       product_array.each do |product|
+  #         product = temp.merge(product)
+  #         csv << CSV::Row.new(product.keys,product.values)
+  #       end
+  #     end
+  #     driver.quit
+  #     puts "#{publisher}.csv is created"
+  # end
 end
 scraper
