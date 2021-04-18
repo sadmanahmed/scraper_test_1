de0e4b545cd99c93987ba1bdf02a3c911fa7b306
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-04-19 01:38:23 +0600

    testing convertion file

--- c.rb
+++ c.rb
@@ -1,21 +1,29 @@
 require 'byebug'
 
-a ="৯০২.৫০ টাকা"
-a.slice!(" টাকা")
-byebug
-z = a.split("")
+def con(a)
 
-b1= { "১"=> "1" ,"২"=> "2"  ,"৩"=> "3" ,"৪"=> "4" ,"৫"=> "5" ,
-  "৬"=> "6" ,"৭"=> "7" ,"৮"=> "8" ,"৯"=> "9" ,"০"=> "0","."=> "."  }
-c=[]
-z.each do |x|
-   y = x.to_s
-   c << b1[y]
-end
-st =""
-c.map do |x|
-  st = st+x
-end
-puts st
+  a.slice!(" টাকা")
+  #byebug
+  z = a.split("")
 
+  b1= { "১"=> "1" ,"২"=> "2"  ,"৩"=> "3" ,"৪"=> "4" ,"৫"=> "5" ,
+    "৬"=> "6" ,"৭"=> "7" ,"৮"=> "8" ,"৯"=> "9" ,"০"=> "0","."=> "."  }
+  c=[]
+  z.each do |x|
+     y = x.to_s
+     c << b1[y]
+  end
+  st =""
+  c.map do |x|
+    st = st+x
+  end
+  st
+end
+
+def test
+  a ="৯০২.৫০ টাকা"
+  x= con(a)
+  puts x
+end
+test
 
